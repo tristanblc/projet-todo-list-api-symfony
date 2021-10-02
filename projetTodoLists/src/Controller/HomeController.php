618ddbd4ff2871003db68ef95bca3c03091c4dfc
tristanblc <tblanc.sio@gmail.com>
2021-10-02 10:25:56 +0200

    #8 ajout du carousel
    -ajout du carousel

--- projetTodoLists/src/Controller/HomeController.php
+++ projetTodoLists/src/Controller/HomeController.php
@@ -2,19 +2,21 @@
 
 namespace App\Controller;
 
-use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use App\Repository\ImagesRepository;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
+use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 
 class HomeController extends AbstractController
 {
     /**
      * @Route("/", name="home")
      */
-    public function index(): Response
+    public function index(ImagesRepository $repo): Response
     {
         return $this->render('home/home.html.twig', [
             'controller_name' => 'HomeController',
+            'lesImages' =>$repo->findAll()
         ]);
     }
 }
